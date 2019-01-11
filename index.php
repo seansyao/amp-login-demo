f381dadf0c1a81cd4814d5efb257996b57b04263
Sean Yao <Sean.Yao@valador.com>
2019-01-11 15:00:27 -0500

    init commit for the code

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php 
+	include ('server.php');
+	//if user is not logged in, they cannot access this page
+	if(empty($_SESSION['username'])) {
+		header('location: login.php');
+	}
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>User Registration System (PHP & MySQL)</title>
+        <link rel="stylesheet" type="text/css" href="./css/style.css">
+    </head>
+    <body>
+        <div class="header">
+            <h2>Homepage</h2>
+        </div>
+        
+        <div class="content">
+        	<?php if(isset($_SESSION['success'])): ?>
+        		<div class="error success">
+        			<h3>
+        				<?php 
+        					echo $_SESSION['success'];
+        					unset($_SESSION['sucess']);
+        				?>
+        			</h3>
+        		</div>
+        	<?php endif ?>
+        	
+        	<?php if(isset($_SESSION['username'])):?>
+        		<p>Welcom <strong><?php echo $_SESSION['username']?></strong></p>
+        		<p><a href="index.php?logout=1" style="color: red;">Logout</a>
+        	<?php endif ?>
+        </div>
+    </body>
+</html>
---
+++ login.php
@@ -0,0 +1,35 @@
+<?php 
+	include('server.php') ;
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>User Registration System (PHP & MySQL)</title>
+        <link rel="stylesheet" type="text/css" href="./css/style.css">
+    </head>
+    <body>
+        <div class="header">
+            <h2>Login</h2>
+        </div>
+        
+        <form method="post" action="login.php">
+        	<!-- display validation errors here -->
+            <?php include('errors.php') ?>
+            
+            <div class="input-group">
+                <label>Username</label>
+                <input type="text" name="username">
+            </div>
+            <div class="input-group">
+                <label>Password</label>
+                <input type="password" name="password">
+            </div>
+            <div class="input-group">
+                <button type="submit" name="login" class="btn">Login</button>
+            </div>
+            <p>
+                Not yet a member? <a href="register.php">Sign Up</a>
+            </p>
+        </form>
+    </body>
+</html>
